bf39b4b3b6451e5bb920ca9bc066dde9c4f20495
Cyril Bouxin <cbouxin@axome.com>
2024-03-22 10:01:30 +0100

    conditions fix (multiple checkbox) / Success modaland some styles

--- modal.js
+++ modal.js
@@ -7,10 +7,9 @@ function editNav() {
   }
 }
 
-// DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalOpenBtn = document.querySelectorAll(".modal-btn");
-const modalCloseBtn = document.querySelector(".close");
+const modalCloseBtns = document.querySelectorAll(".close");
 
 const formData = document.querySelectorAll(".formData");
 const form = document.querySelector("form");
@@ -23,6 +22,8 @@ let errorList = {
   birthdate: null,
   quantity: null,
   location: null,
+  checkbox1: null,
+  checkbox2: null,
 };
 
 let data = {
@@ -32,13 +33,15 @@ let data = {
   birthdate: Date,
   quantity: Number,
   location: String,
+  checkbox1: Boolean,
+  checkbox2: Boolean,
 };
 
-// open and close modal event
 modalOpenBtn.forEach((btn) => btn.addEventListener("click", openModal));
-modalCloseBtn.addEventListener("click", closeModal);
+modalCloseBtns.forEach((button) => {
+  button.addEventListener("click", closeModal);
+});
 
-// Form submit event
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   resetData();
@@ -48,130 +51,181 @@ form.addEventListener("submit", (e) => {
 // Form submit function
 function handleSubmit() {
   formData.forEach((formDataElement) => {
-    let input = formDataElement.querySelector("input");
-    let isValid;
+    let inputs = formDataElement.querySelectorAll("input");
+    let isValid = false;
     let error;
 
-    /* Retrait des erreurs sur l'élément */
+    // Reset des erreurs sur l'élément
     removeError(formDataElement);
 
-    switch (input.name) {
-      case "first":
-        if (input.value) {
-          if (input.value.length > 0) {
-            isValid = validateFirstName(input.value);
-            if (isValid) {
-              data.first = input.value;
-              return;
+    // Boucle sur les inputs afin de vérifier et extraire leur valeur
+    inputs.forEach((input) => {
+      switch (input.name) {
+        case "first":
+          if (input.value) {
+            if (input.value.length > 0) {
+              isValid = validateFirstAndLastName(input.value);
+              if (isValid) {
+                data.first = input.value;
+                return;
+              } else {
+                error = "Prénom non valide";
+              }
             } else {
-              error = "Prénom non valide";
+              error = "Prénom trop court";
             }
           } else {
-            error = "Prénom trop court";
+            error = "Veuillez compléter le champ";
           }
-        } else {
-          error = "Veuillez compléter le champ";
-        }
-        setError(formDataElement, error);
-        break;
+          setError(formDataElement, error);
+          break;
 
-      case "last":
-        if (input.value) {
-          if (input.value.length > 0) {
-            isValid = validateLastName(input.value);
-            if (isValid) {
-              data.first = input.value;
-              return;
+        case "last":
+          if (input.value) {
+            if (input.value.length > 0) {
+              isValid = validateFirstAndLastName(input.value);
+              if (isValid) {
+                data.last = input.value;
+                return;
+              } else {
+                error = "Nom de famille non valide";
+              }
             } else {
-              error = "Nom de famille non valide";
+              error = "Nom de famille trop court";
             }
           } else {
-            error = "Nom de famille trop court";
+            error = "Veuillez compléter le champ";
           }
-        } else {
-          error = "Veuillez compléter le champ";
-        }
-        setError(formDataElement, error);
-        break;
+          setError(formDataElement, error);
+          break;
 
-      case "birthdate":
-        if (input.value) {
-          let birthDate = new Date(input.value);
-          let currentDate = new Date();
-          let minDate = new Date(currentDate);
-          let maxDate = new Date(currentDate);
+        case "email":
+          if (input.value) {
+            isValid = validateEmail(input.value);
+            if (isValid) {
+              data.email = input.value;
+              return;
+            } else {
+              error = "Adresse email invalide";
+            }
+          } else {
+            error = "Veuillez compléter le champ";
+          }
+          setError(formDataElement, error);
+          break;
+        case "birthdate":
+          if (input.value) {
+            let birthDate = new Date(input.value);
+            let currentDate = new Date();
+            let minDate = new Date(currentDate);
+            let maxDate = new Date(currentDate);
 
-          // Âge minimum de 16 ans
-          minDate.setFullYear(currentDate.getFullYear() - 16);
-          // Âge maximum de 100 ans
-          maxDate.setFullYear(currentDate.getFullYear() - 100);
+            // Age minimum de 16 ans
+            minDate.setFullYear(currentDate.getFullYear() - 16);
+            // Age maximum de 100 ans
+            maxDate.setFullYear(currentDate.getFullYear() - 100);
 
+            if (
+              !isNaN(birthDate.getTime()) &&
+              birthDate.getTime() <= minDate.getTime() &&
+              birthDate.getTime() >= maxDate.getTime()
+            ) {
+              data.birthdate = input.value;
+              return;
+            } else {
+              error = "Il faut avoir plus de 16 ans pour s'inscrire";
+            }
+          } else {
+            error = "Veuillez compléter le champ";
+          }
+          setError(formDataElement, error);
+          break;
+
+        case "quantity":
+          // La valeur doit être un nombre et ne pas être négatif //
           if (
-            !isNaN(birthDate.getTime()) &&
-            birthDate.getTime() <= minDate.getTime() &&
-            birthDate.getTime() >= maxDate.getTime()
+            input.value &&
+            !isNaN(input.value) &&
+            parseInt(input.value) >= 0
           ) {
             isValid = true;
-            data.birthdate = input.value;
-			return;
+            data.quantity = parseInt(input.value);
+            return;
           } else {
-            error =
-              "Il faut avoir plus de 16 ans pour s'inscrire";
+            error = "Veuillez saisir une quantité valide";
           }
-        } else {
-          error = "Veuillez compléter le champ";
-        }
-        setError(formDataElement, error);
-        break;
+          setError(formDataElement, error);
+          break;
 
-      case "quantity":
-        if (input.value && !isNaN(input.value)) {
-          isValid = true;
-          data.quantity = parseInt(input.value);
-        } else {
-          error = "Veuillez saisir une quantité valide";
-        }
-        break;
-      case "location":
-        let checkedLocations = document.querySelectorAll(
-          'input[name="location"]:checked'
-        );
-        if (checkedLocations.length > 0) {
-          isValid = true;
-          data.location = checkedLocations[0].value;
-          return;
-        } else {
-          error = "Veuillez choisir une location";
-        }
-        setError(formDataElement, error);
-        break;
-      default:
-        break;
-    }
+        case "location":
+          let checkedLocations = document.querySelectorAll(
+            'input[name="location"]:checked'
+          );
+          if (checkedLocations.length > 0) {
+            data.location = checkedLocations[0].value;
+            return;
+          } else {
+            error = "Veuillez choisir un tournoi";
+          }
+          setError(formDataElement, error);
+          break;
+
+        case "checkbox1":
+          if (input.checked) {
+            data.checkbox1 = true;
+            return;
+          } else {
+            error = "Vous devez accepter les conditions d'utilisation";
+          }
+          setError(formDataElement, error);
+          break;
+
+        case "checkbox2":
+          data.checkbox2 = input.checked;
+          break;
+
+        default:
+          break;
+      }
+    });
   });
-  console.log(data);
+
+  isValid = validateLastStep(data);
+
+  if (isValid) {
+    let modalForm = document.querySelector(".modal-body form");
+	modalForm.classList.add('thanks');
+    modalForm.innerHTML = "<p>Merci pour votre inscription<p>";
+	document.querySelector(".thanks-close-btn").style.display = "block"
+
+	
+  }
 }
 
-// Validate functions
-function validateFirstName(firstName) {
-  if (firstName.length <= 30) {
-    return /^[a-zA-Z]+(?:-[a-zA-Z]+){0,2}$/.test(firstName);
+// Permet la vérification de l'adresse email //
+function validateFirstAndLastName(name) {
+  if (name.length <= 30) {
+    return /^[a-zA-Z]+(?:-[a-zA-Z]+){0,2}$/.test(name);
   }
   return false;
 }
 
-function validateLastName(lastName) {
-  if (lastName.length <= 30) {
-    return /^[a-zA-Z]+(?:-[a-zA-Z]+){0,2}$/.test(lastName);
-  }
-  return false;
-}
-
+// Permet la vérification de l'adresse email //
 function validateEmail(email) {
   if (email.length <= 255) {
     return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
   }
   return false;
+}
+
+// Permet la vérification de l'objet data avant l'envoi des données //
+function validateLastStep(element) {
+  for (const key in element) {
+    if (element.hasOwnProperty(key) && element[key] === null) {
+      return false;
+    }
+  }
+  return true;
 }
 
 // launch modal form
@@ -190,6 +244,8 @@ function resetData() {
   data.birthdate = null;
   data.quantity = null;
   data.location = null;
+  data.checkbox1 = null;
+  data.checkbox2 = null;
 }
 
 function setError(element, error) {
